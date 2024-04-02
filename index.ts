82a86f3cd7e087ef4b712a68411fc781b44a2779
MUHAMMAD USMAN <162905776+STARUSMAN@users.noreply.github.com>
2024-04-02 23:40:14 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1 );
+
+// console.log(randomNumber);
+
+const answer = await inquirer.prompt([{
+    name: "userGuessNumber",
+    type: "number",
+    message: "please guess a number between 1-10: ",
+},
+]);
+
+if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulation! you guessed right number")
+}else{
+    console.log("you guessed wrong number")
+}
+
+console.log(answer);
